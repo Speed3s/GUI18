3bee50577a45b29cd6eaaaa44556494c013228b7
anwar50 <37552972+anwar50@users.noreply.github.com>
2019-03-04 00:15:50 +0000

    Add files via upload

--- we/ImageComponents.js
+++ we/ImageComponents.js
@@ -10,12 +10,15 @@ import Weather from "./Weather.js"
 
 const Api_Key = "8d2de98e089f1c28e1a22fc19a24ef04";
 let arr = []
+let arrtemp = []
+let count = 0
 class ImageComponent extends React.Component{
     
     state = {
-
+        
         description: undefined,
         city: undefined,
+        temperature: undefined,
         country: undefined,
         error: undefined
 
@@ -34,48 +37,59 @@ class ImageComponent extends React.Component{
                     if(subitem.description === "clear sky" && condition === "sun")
                     {
                         this.setState({
+                            
                             description: subitem.description,
+                            temperature: item.main.temp,
                             city: item.name,
                             country: item.sys.country,
                             error: ""
                         })
+                        arrtemp.push(item.main.temp);
                         arr.push(item.name);
                     }
                     else if(((subitem.description === "broken clouds") || (subitem.description === "few clouds") || (subitem.description === "scattered clouds")) && condition === "cloud")
                     {
                         this.setState({
                             description: subitem.description,
+                            temperature: item.main.temp,
                             city: item.name,
                             country: item.sys.country,
                             error: ""
                         })
+                        arrtemp.push(item.main.temp);
                         arr.push(item.name);
                     }
                     else if(subitem.description === "rain" && condition === "rain"){
                         this.setState({
                             description: subitem.description,
+                            temperature: item.main.temp,
                             city: item.name,
                             country: item.sys.country,
                             error: ""
                         })
+                        arrtemp.push(item.main.temp);
                         arr.push(item.name);
                     }
                     else if(subitem.description === " showerrain" && condition === "rain"){
                         this.setState({
                             description: subitem.description,
+                            temperature: item.main.temp,
                             city: item.name,
                             country: item.sys.country,
                             error: ""
                         })
+                        arrtemp.push(item.main.temp);
                         arr.push(item.name);
                     }
                     else if(subitem.description === "snow" && condition === "snow"){
                         this.setState({
                             description: subitem.description,
+                            temperature: item.main.temp,
                             city: item.name,
                             country: item.sys.country,
                             error: ""
                         })
+                        arrtemp.push(item.main.temp);
                         arr.push(item.name);
                     }
                 }));
@@ -140,30 +154,48 @@ class ImageComponent extends React.Component{
         }
         
         return(
-            <div>
-                <img className="Logo" src={logo} alt="Logo" />
+            <div className="fullcontainer">
+            <div className="container">
+                {/* <img className="Logo" src={logo} alt="Logo" />
                 <div className="HeaderOuter">
                     <div className="HeaderWrapper1">
                             <p>Weather-to-go</p>
                     </div>
-                </div>
-                    <div id="ImageWrap" >
-                        <div id="ImageOuter">
-                            <div id="ImageWrapper1">
-                            <button className="btn" onClick={this.handleSubmit} name="snow" value = "snow"><img className="weatherComp" style={Size} src={snowy} alt="snowy"/></button>
-                            </div>
-                            <div id="ImageWrapper2">
-                            <button className="btn" onClick={this.handleSubmit} name="rain" value = "rain" ><img className="weatherComp" style= {Size} src={rainy} alt="rainy" /></button>
-                            </div>
-                            <div id="ImageWrapper3">
-                            <button className="btn" onClick={this.handleSubmit} name="cloud" value = "cloud"><img className="weatherComp" style= {Size} src={cloudy} alt="cloudy" /></button>
-                            </div>
-                            <div id="ImageWrapper4">
-                            <button className="btn" onClick={this.handleSubmit} name="sun" value = "sun"><img className="weatherComp" style={Size} src={sunny} alt="sunny"/></button>
+                </div> */}
+                        <div className="row">
+                            <div className="col-25">
+                                <button className="btn" onClick={this.handleSubmit} name="snow" value = "snow"><img className="weatherComp" style={Size} src={snowy} alt="snowy"/></button>
                             </div>
                         </div>
-                    </div>  
+
+                        <div className="row">
+                                <div className="col-25">
+                                    <button className="btn" onClick={this.handleSubmit} name="rain" value = "rain" ><img className="weatherComp" style= {Size} src={rainy} alt="rainy" /></button>
+                                </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-25">
+                                <button className="btn" onClick={this.handleSubmit} name="cloud" value = "cloud"><img className="weatherComp" style= {Size} src={cloudy} alt="cloudy" /></button>
+                            </div>
+                        </div>
+
+                        <div className="row">
+                            <div className="col-25">
+                                <button className="btn" onClick={this.handleSubmit} name="sun" value = "sun"><img className="weatherComp" style={Size} src={sunny} alt="sunny"/></button>
+                            </div>
+                        </div>
                 </div>
+                <div className="container2">
+                    <div><Weather 
+                    
+                    count = {arr.length}
+                    cities={arr} 
+                    temp = {arrtemp}
+                    
+                    /></div>
+                </div>
+            </div>
              
         )
     }
--- we/Weather.js
+++ we/Weather.js
@@ -1,17 +1,59 @@
 import React from "react";
 
-function Weather({ cities }) {
+function Weather({ cities, count, temp }) {
     return (
-        <div >
-            {
-                cities.map(item => (
-                <div>
-                    <h4>{item}</h4>
-                </div>))
-            }
+        <div>
+            
+            <div >
+                {
+                    cities.map((item, i) => (
+                        item && <p> {i+1}: 
+                    <span>
+                        {item}
+                    </span></p>))
+                    
+                }
+                {/* {
+                    temp.map((item, i) => (
+                        item && <p> temp: 
+                    <span>
+                        {item}
+                    </span></p>))
+                } */}
+                {
+                    count && <p>Number Of Countries: 
+                        <span>  {count}</span>
+                    </p>
+                }
+            </div>
+        
         </div>
     );
 }
+
+// class Weather extends React.Component{
+
+//     render(){
+
+//         return(
+
+//             <div>
+//                 {
+//                     this.props.country && this.props.city && <p className="weather__key">Location: 
+//                         <span className="weather__value">  {this.props.city}, {this.props.country}</span>                    
+//                     </p> 
+//                 }
+                
+//                 {
+//                     this.props.count && <p>count: 
+//                         <span>  {this.props.count}</span>
+//                     </p>
+//                 }
+//             </div>
+//         )
+//     }
+// }
+
 
 
 
