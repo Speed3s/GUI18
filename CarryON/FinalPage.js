1d5a35a337b822284181d8f04910146d1ca99115
Speed3s <44297378+Speed3s@users.noreply.github.com>
2019-03-06 04:04:27 +0000

    Update FinalPage.js

--- CarryON/FinalPage.js
+++ CarryON/FinalPage.js
@@ -9,24 +9,6 @@ class FinalPage extends Component{
     
     render(){
         return(
-            <div>                                    
-                {(ImageComponent.state.country || []).map( showCountries => (
-                    <ul> {showCountries} </ul>
-                ))}
-            
-                {(ImageComponent.state.city || []).map( showCity => (
-                    <ul> {showCity} </ul>
-                ))} 
-
-                {(ImageComponent.state.temperature || []).map( showTemperature => (
-                    <ul> {showTemperature} </ul>
-                ))}  
-
-                {(ImageComponent.state.humidity || []).map( showHumidity => (
-                    <ul> {showHumidity} </ul>
-                ))}                           
-
-            </div>
         )
     }
 }
